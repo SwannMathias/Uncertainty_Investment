f6c8ec7a6741a8a4ca52002e7f787c2f243e9c3b
schelly <swann.chelly@insee.fr>
2026-03-03 14:18:06 +0000

    Remove all test, update sanity checks

--- scripts/no_uncertainty_fixed_cost.jl
+++ scripts/no_uncertainty_fixed_cost.jl
@@ -6,9 +6,9 @@ project_root = dirname(@__DIR__)
 Pkg.activate(project_root)
 using UncertaintyInvestment
 using Printf
-
+using NPZ
 using Distributed
-addprocs(70)
+
 using Random
 
 ## 1. No uncertainty, Fixed cost
@@ -38,17 +38,19 @@ params = ModelParameters(
     ),
     
     numerical = NumericalSettings(
-        n_K = 100,
-        n_D = 5,
-        n_sigma = 3,    # Can reduce to 1 since σ ≈ 0
+        n_K = 3000,
+        n_D = 2,
+        n_sigma = 2,    # Can reduce to 1 since σ ≈ 0
         K_min_factor = 0.1,
         K_max_factor = 10.0,
         tol_vfi = 1e-4,
         max_iter = 200,
-        howard_steps = 5
+        howard_steps = 0
     )
 )
 
+
+NPZ.npzwrite("output/simulations/grid_K.npy",construct_grids(params).K_grid)
 ac_begin = FixedAdjustmentCost(F = 0.1)
 ac_mid_year= FixedAdjustmentCost(F = 0.1)
 sol_scenario1 = solve_model(params; ac_begin=ac_begin,ac_mid_year = ac_mid_year, verbose=true,use_parallel=true, use_multiscale=true)
@@ -74,6 +76,8 @@ histories = simulate_firm_panel(
 )
 panel = construct_estimation_panel(histories)
 save_simulation("output/simulations/panel_data_s11.csv", panel)
+NPZ.npzwrite("output/simulations/I_policy_s11.npy",sol_scenario1.I_policy)
+NPZ.npzwrite("output/simulations/Delta_I_policy_s11.npy",sol_scenario1.Delta_I_policy)
 
 histories = simulate_firm_panel(
     sol_scenario2,
@@ -83,6 +87,8 @@ histories = simulate_firm_panel(
 )
 panel = construct_estimation_panel(histories)
 save_simulation("output/simulations/panel_data_s12.csv", panel)
+NPZ.npzwrite("output/simulations/I_policy_s12.npy",sol_scenario2.I_policy)
+NPZ.npzwrite("output/simulations/Delta_I_policy_s12.npy",sol_scenario2.Delta_I_policy)
 ## 2. No uncertainty, Different fixed cost
 # Increasing the fixed cost should decrease the frequency of investment. 
 # Set seed for reproducibility
@@ -111,6 +117,8 @@ histories = simulate_firm_panel(
 )
 panel = construct_estimation_panel(histories)
 save_simulation("output/simulations/panel_data_s21.csv", panel)
+NPZ.npzwrite("output/simulations/I_policy_s21.npy",sol_scenario1.I_policy)
+NPZ.npzwrite("output/simulations/Delta_I_policy_s21.npy",sol_scenario1.Delta_I_policy)
 
 histories = simulate_firm_panel(
     sol_scenario2,
@@ -120,4 +128,7 @@ histories = simulate_firm_panel(
 )
 panel = construct_estimation_panel(histories)
 save_simulation("output/simulations/panel_data_s22.csv", panel)
+NPZ.npzwrite("output/simulations/I_policy_s22.npy",sol_scenario2.I_policy)
+NPZ.npzwrite("output/simulations/Delta_I_policy_s22.npy",sol_scenario2.Delta_I_policy)
+
 
